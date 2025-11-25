48002c0236aedf5d40e58763b6019a658f958516
Zebreus <zebreus@zebre.us>
2025-11-25 15:39:59 +0100

    Finalize context switching api

--- libc-top-half/musl/src/wasix/context.c
+++ libc-top-half/musl/src/wasix/context.c
@@ -3,24 +3,40 @@
 #include <wasi/api_wasix.h>
 #include <wasix/context.h>
 
-// Points to the last context.
-wasix_context_id_t context_main_context;
-
-int wasix_context_new(
-    wasix_context_id_t* new_context_ptr,
-    void (*entrypoint_fn)()
-) {
-    return __wasi_context_new(new_context_ptr, (__wasi_function_pointer_t)entrypoint_fn);
+wasix_context_id_t __wasix_context_main(void) {
+  return 0; // Main context is always ID 0 for now
 }
 
-int wasix_context_switch(
-    wasix_context_id_t next_context
-) {
-    return __wasi_context_switch(next_context);
+int wasix_context_create(wasix_context_id_t *context_id, void (*entrypoint)()) {
+  int err =
+      __wasi_context_create(context_id, (__wasi_function_pointer_t)entrypoint);
+
+  if (err != __WASI_ERRNO_SUCCESS) {
+    errno = err;
+    return -1;
+  }
+
+  return 0;
 }
 
-int wasix_context_delete(
-    wasix_context_id_t context
-) {
-    return __wasi_context_delete(context);
+int wasix_context_switch(wasix_context_id_t target_context_id) {
+  int err = __wasi_context_switch(target_context_id);
+
+  if (err != __WASI_ERRNO_SUCCESS) {
+    errno = err;
+    return -1;
+  }
+
+  return 0;
+}
+
+int wasix_context_destroy(wasix_context_id_t context_id) {
+  int err = __wasi_context_destroy(context_id);
+
+  if (err != __WASI_ERRNO_SUCCESS) {
+    errno = err;
+    return -1;
+  }
+
+  return 0;
 }

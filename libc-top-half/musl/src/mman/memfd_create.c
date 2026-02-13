5e49e07d7dd672f8763c4393b4c0128e17deeba0
Zebreus <zebreus@zebre.us>
2026-02-13 11:47:13 +0100

    Add name to memfd_create

--- libc-top-half/musl/src/mman/memfd_create.c
+++ libc-top-half/musl/src/mman/memfd_create.c
@@ -1,10 +1,15 @@
 #define _GNU_SOURCE
 
+#include <ctype.h>
 #include <errno.h>
 #include <fcntl.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/mman.h>
 #include <unistd.h>
+
+#define MEMFD_NAME_MAX 249
 
 static int create_unlinked_temp(char *template_path, unsigned int flags)
 {
@@ -26,10 +31,33 @@ static int create_unlinked_temp(char *template_path, unsigned int flags)
 	return fd;
 }
 
+// Returns the length of the name without the null character.
+// If the returned length == dst_len, the src was too long. dst is not null terminated in this case.
+static int sanitize_name(char *dst, size_t dst_len, const char *src)
+{
+	size_t i = 0;
+
+	while (i < dst_len && src[i] != '\0') {
+		unsigned char c = (unsigned char)src[i];
+		if (isalnum(c) || c == '_' || c == '-' || c == '.') {
+			dst[i] = (char)c;
+		} else {
+			dst[i] = '_';
+		}
+		i++;
+	}
+
+	if (i == 0 && dst_len > 1) {
+		strcpy(dst, "anon");
+		return 4;
+	}
+
+	return i;
+}
+
+
 int memfd_create(const char *name, unsigned int flags)
 {
-	(void)name;
-
 	if (flags & ~(MFD_CLOEXEC | MFD_ALLOW_SEALING | MFD_HUGETLB)) {
 		errno = EINVAL;
 		return -1;
@@ -42,10 +70,25 @@ int memfd_create(const char *name, unsigned int flags)
 		return -1;
 	}
 
-	char tmp_template[] = "/tmp/.memfd-XXXXXX";
+	if (name == NULL) {
+		errno = EFAULT;
+		return -1;
+	}
+
+	// Construct a string like "/tmp/.memfd-<name>-XXXXXX"
+	char tmp_template[sizeof("/tmp/.memfd-")-1 + MEMFD_NAME_MAX + sizeof("-XXXXXX")-1 + 1];
+	memcpy(tmp_template, "/tmp/.memfd-", sizeof("/tmp/.memfd-"));
+	int name_len = sanitize_name(tmp_template + sizeof("/tmp/.memfd-") - 1, MEMFD_NAME_MAX + 1, name);
+	if (name_len > MEMFD_NAME_MAX) {
+		errno = EINVAL;
+		return -1;
+	}
+	memcpy(tmp_template + sizeof("/tmp/.memfd-") - 1 + name_len, "-XXXXXX", sizeof("-XXXXXX"));
+
 	int fd = create_unlinked_temp(tmp_template, flags);
 	if (fd >= 0) return fd;
 
-	char cwd_template[] = "./.memfd-XXXXXX";
-	return create_unlinked_temp(cwd_template, flags);
+	// Use ".memfd-<name>-XXXXXX" to create a temporary file in the current working directory.
+	char* cwd_name = tmp_template + sizeof("/tmp/") - 1;
+	return create_unlinked_temp(cwd_name, flags);
 }

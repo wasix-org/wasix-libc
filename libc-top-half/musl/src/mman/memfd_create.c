8925fa0622e2acaf6077e284b5dfc5cd54d445aa
Zebreus <zebreus@zebre.us>
2026-02-13 09:30:05 +0100

    Implement memfs_create

---
+++ libc-top-half/musl/src/mman/memfd_create.c
@@ -0,0 +1,57 @@
+#define _GNU_SOURCE
+
+#include <errno.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <sys/mman.h>
+#include <unistd.h>
+
+static int create_unlinked_temp(char *template_path, unsigned int flags)
+{
+	int fd;
+	if (flags & MFD_CLOEXEC) {
+		fd = mkostemp(template_path, O_CLOEXEC);
+	} else {
+		fd = mkstemp(template_path);
+	}
+	if (fd < 0) return -1;
+
+	if (unlink(template_path) != 0) {
+		int saved_errno = errno;
+		close(fd);
+		errno = saved_errno;
+		return -1;
+	}
+
+	if ((flags & MFD_CLOEXEC) && fcntl(fd, F_SETFD, FD_CLOEXEC) != 0) {
+		int saved_errno = errno;
+		close(fd);
+		errno = saved_errno;
+		return -1;
+	}
+
+	return fd;
+}
+
+int memfd_create(const char *name, unsigned int flags)
+{
+	(void)name;
+
+	if (flags & ~(MFD_CLOEXEC | MFD_ALLOW_SEALING)) {
+		errno = EINVAL;
+		return -1;
+	}
+
+	/* Seals are not currently supported by the WASIX fcntl subset. */
+	if (flags & MFD_ALLOW_SEALING) {
+		errno = ENOTSUP;
+		return -1;
+	}
+
+	char tmp_template[] = "/tmp/.memfd-XXXXXX";
+	int fd = create_unlinked_temp(tmp_template, flags);
+	if (fd >= 0) return fd;
+
+	char cwd_template[] = "./.memfd-XXXXXX";
+	return create_unlinked_temp(cwd_template, flags);
+}

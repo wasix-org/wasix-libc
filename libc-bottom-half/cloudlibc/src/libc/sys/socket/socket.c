75bd05580fc4df3d87c7a13428053ac1ce3d8de4
Sadhbh Code <sonia.kolasinska.pro@gmail.com>
2026-06-05 15:26:02 +0100

    Open sockets with nonblock|cloexec flags correctly

--- libc-bottom-half/cloudlibc/src/libc/sys/socket/socket.c
+++ libc-bottom-half/cloudlibc/src/libc/sys/socket/socket.c
@@ -5,11 +5,15 @@
 #include <wasi/api.h>
 #include <errno.h>
 #include <string.h>
+#include <unistd.h>
 
 int socket(int domain, int ty, int protocol) {
   int fd;
+  int flags = ty & (SOCK_NONBLOCK | SOCK_CLOEXEC);
+  int socktype = ty & ~(SOCK_NONBLOCK | SOCK_CLOEXEC);
+
   if(!protocol) {
-    switch (ty)
+    switch (socktype)
     {
     case SOCK_STREAM:
       protocol = IPPROTO_TCP;
@@ -19,10 +23,33 @@ int socket(int domain, int ty, int protocol) {
       break;
     }
   }
-  __wasi_errno_t error = __wasi_sock_open(domain, ty, protocol, &fd);
+  __wasi_errno_t error = __wasi_sock_open(domain, socktype, protocol, &fd);
   if (error != 0) {
     errno = error;
     return -1;
+  }
+
+  if (flags & SOCK_NONBLOCK) {
+    __wasi_fdstat_t fds;
+    error = __wasi_fd_fdstat_get(fd, &fds);
+    if (error == 0) {
+      error = __wasi_fd_fdstat_set_flags(
+          fd, fds.fs_flags | __WASI_FDFLAGS_NONBLOCK);
+    }
+    if (error != 0) {
+      close(fd);
+      errno = error;
+      return -1;
+    }
+  }
+
+  if (flags & SOCK_CLOEXEC) {
+    error = __wasi_fd_fdflags_set(fd, __WASI_FDFLAGSEXT_CLOEXEC);
+    if (error != 0) {
+      close(fd);
+      errno = error;
+      return -1;
+    }
   }
 
   return fd;
